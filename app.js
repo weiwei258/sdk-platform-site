558b0272bf8ea696db6355633cc12633a82d70ad
weiwei <2491812393@qq.com>
2024-03-03 21:02:08 +0800

    feat(deployment): 修改打包相关

--- app.js
+++ app.js
@@ -12,7 +12,4 @@ app.use(static(staticPath));
 
 // 启动服务器，监听端口
 const port = 7002; // 可以根据需要修改端口号
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at ${port}/`);
-});
-
+app.listen(port, 'localhost');
