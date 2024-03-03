f2efbea62e803a8955ee962f67bedf9babcd5920
weiwei <2491812393@qq.com>
2024-03-03 20:50:43 +0800

    feat(deployment): 修改打包相关

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = new Koa();
 
 // 设置静态文件夹的路径为 dist
-const staticPath = path.join(__dirname, 'dist');
+const staticPath = path.join(__dirname, './dist');
 
 // 使用 koa-static 中间件
 app.use(static(staticPath));
@@ -15,3 +15,4 @@ const port = 7002; // 可以根据需要修改端口号
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at ${port}/`);
 });
+
