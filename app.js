10d07b28072cecd37daffb9438dc8934b4ab6e9d
weiwei <2491812393@qq.com>
2024-03-03 19:13:00 +0800

    feat(deployment): 增加部署相关功能

---
+++ app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const path = require('path');
+
+const app = new Koa();
+
+// 设置静态文件夹的路径为 dist
+const staticPath = path.join(__dirname, 'dist');
+
+// 使用 koa-static 中间件
+app.use(static(staticPath));
+
+// 启动服务器，监听端口
+const port = 7002; // 可以根据需要修改端口号
+app.listen(port, () => {
+  console.log(`Server running at ${port}/`);
+});
